3a10aa4179d16381547f54d5cd13d13d3329b5ef
Qadees Javed <qadeesjaved9098@gmail.com>
2020-06-02 20:07:27 +0500

    updated by qadees and show github profile info

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Hi, I'm Qadees Javed</h3>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+     axios.get(`https://api.github.com/users/qadeesz`)
+      .then(res => {
+        const users = [res.data];
+        console.log(users)
+        this.setState({ users: users });
+        console.log(this.state.users)
+      })
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>Updated by Qadees Javed</h3>
+          <p>
+          { this.state.users.map(user => 
+                
+                <div className="card">
+                  <img src={user.avatar_url} alt="Avatar" />
+                  <div className="container">
+                    <h4><b>{user.name}</b></h4> 
+                    <p>{user.bio}</p> 
+                    <p className="App-link">
+                      <a href={user.html_url} target="_blank">Github Link</a> 
+                    </p>
+                  </div>
+                </div>
+               
+          )}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
